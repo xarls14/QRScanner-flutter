c170bca903608134daff34c05c580ad1396173b4
Henriquez <ch250338@ncr.com>
2019-08-22 18:25:57 -0400

    commit qrreaderapp - creación de base de datos sqlite

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:qrcode_reader/qrcode_reader.dart';
+import 'package:qrreaderapp/src/bloc/scans_bloc.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'direcciones_page.dart';
 import 'mapas_page.dart';
 
@@ -12,6 +14,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  final scansBloc = new ScansBloc();
+
   int currentIndex = 0;
 
   @override
@@ -22,7 +26,7 @@ class _HomePageState extends State<HomePage> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.delete_forever),
-            onPressed: (){},
+            onPressed: scansBloc.borrarScansTODOS,
           )
         ],
       ),
@@ -31,10 +35,42 @@ class _HomePageState extends State<HomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.filter_center_focus),
-        onPressed: (){},
+        onPressed: _scanQR,
         backgroundColor: Theme.of(context).primaryColor,
       ),
     );
+  }
+
+   _scanQR() async{
+
+    //https://fernando-herrera.com
+    //
+
+    String futureString = 'https://fernando-herrera.com';
+    
+    /*
+
+    try {
+      futureString = await new QRCodeReader().scan();
+    } catch (e) {
+      futureString = e.toString();
+    }
+
+    */
+
+    if (futureString != null) {
+
+      final scan = ScanModel( valor: futureString );
+
+      scansBloc.agregarScan(scan);
+
+        final scan2 = ScanModel( valor: 'geo:40.724233047051705,-74.00731459101564' );
+
+      scansBloc.agregarScan(scan2);
+    }
+
+
+
   }
 
   Widget _callPage(int paginaActual){
--- lib/src/pages/mapas_page.dart
+++ lib/src/pages/mapas_page.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/bloc/scans_bloc.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 
 class MapasPage extends StatelessWidget {
 
+  final scansBloc = new ScansBloc();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mapa Page'),
+
+    
+
+    return StreamBuilder<List<ScanModel>>(
+      stream: scansBloc.scansStream,
+      builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+        
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator(),);
+        }
+
+        final scans = snapshot.data;
+
+        if (scans.length == 0) {
+          return Center(child: Text('No hay info! '),);
+        }
+
+        return ListView.builder(
+          itemCount: scans.length,
+          itemBuilder: (context, i) => Dismissible(
+            background: Container(
+              color: Colors.red,
+            ),
+            onDismissed: (direction) => scansBloc.borrarScan(scans[i].id),
+            key: UniqueKey(),
+            child: ListTile(
+              leading: Icon(Icons.cloud_queue, color: Theme.of(context).primaryColor,),
+              title: Text(scans[i].valor),
+              subtitle: Text('ID: ${scans[i].id}'),
+              trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+            ),
+          )
+          
+          
+        );
+
+      },
     );
   }
 }
